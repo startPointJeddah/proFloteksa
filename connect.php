b47af8b04a7c9f5308f4881f28cc5a664413517f
MohamedSaleh <mohamed@startpoint.com>
2022-10-15 14:27:25 +0300

    final push

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'projectdb';
+$username = 'u191962684_prodb';
+$password = 'QnTOR7zDOk8mgJ1!';
+$dbname = 'u191962684_prodb ';
 $conn=new mysqli($servername ,$username ,$password ,$dbname );
 $conn->set_charset('utf8');
 
